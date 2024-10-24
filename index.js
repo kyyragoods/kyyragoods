e83673f6405c609ffcdc55a8c3a1542277914836
kyyragoods <154533562+kyyragoods@users.noreply.github.com>
2024-10-24 22:07:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Vercel!');
+  res.send('alooo raraa entutt🦆💨');
 });
 
 const port = process.env.PORT || 3000;
